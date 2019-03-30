c06b8be840760fb02c61c02f38e3af20e96065b0
Ronak Shah <ronakshahultimate@gmail.com>
2019-03-30 09:22:17 -0700

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import logo from './logo.svg'
+import './App.css'
+import Index from './components/Index.js'
+import Login from './components/Login.js'
+import Dashboard from './components/Dashboard.js'
+
+class Header extends Component {
+  render() {
+    return (
+      <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+    </ul>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div>
+        <Header />
+
+        <Route exact path="/" component={Index} />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
       </div>
+    </Router>
     );
   }
 }
